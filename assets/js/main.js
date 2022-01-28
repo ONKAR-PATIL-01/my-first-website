095e5f83acdbe137cb0e047156f7130905c00ba8
Onkar Patil <2020bcs504@sggs.ac.in>
2022-01-28 14:08:10 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+
+
+(function($) {
+
+	skel.breakpoints({
+		xlarge: '(max-width: 1680px)',
+		large: '(max-width: 1280px)',
+		medium: '(max-width: 980px)',
+		small: '(max-width: 736px)',
+		xsmall: '(max-width: 480px)'
+	});
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body');
+
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 100);
+			});
+
+			$('form').placeholder();
+
+			skel.on('+medium -medium', function() {
+				$.prioritize(
+					'.important\\28 medium\\29',
+					skel.breakpoint('medium').active
+				);
+			});
+
+	
+			$('.scrolly').scrolly();
+
+
+	});
+
+})(jQuery);
